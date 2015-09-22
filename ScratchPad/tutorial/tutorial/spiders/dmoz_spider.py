bd07e79e22cfe59f4e86eb69be371c7e80f29444
Huiru <huiru.ru@gmail.com>
2015-09-22 01:37:25 -0400

    more experiments with scrapy

--- ScratchPad/tutorial/tutorial/spiders/dmoz_spider.py
+++ ScratchPad/tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,15 +1,23 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from tutorial.items import DmozItem
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
-    allowed_domains = ["dmoz.org"]
+    allowed_domains = ["poets.org"]
     start_urls = [
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Books/",
-        "http://www.dmoz.org/Computers/Programming/Languages/Python/Resources/"
-    ]
+    		"http://www.poets.org/poetsorg/poets"
+	    ]
 
     def parse(self, response):
-        filename = response.url.split("/")[-2] + '.html'
-        with open(filename, 'wb') as f:
-            f.write(response.body)
+    	for href in response.css("ul.directory.dir-col > li > a::attr('href')"):
+    		url = response.urljoin(href.extract())
+    		yield scrapy.Request(url, callback=self.parse_dir_contents)
+
+    def parse(self, response):
+    	for sel in response.xpath('//ul/li'):
+    		item = DmozItem()
+    		item['title'] = sel.xpath('a/text()').extract()
+    		item['link'] = sel.xpath('a/@href').extract()
+    		item['desc'] = sel.xpath('text()').extract()
+    		yield item
